4564051c37c04d316d0d358fab71868a32f2b2e3
asmasyakirah <asma.syakirahd@gmail.com>
2020-04-23 09:40:47 +0800

    use real data and complete functional part

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ChartDataSets } from 'chart.js';
 import { Color, Label } from 'ng2-charts';
@@ -6,13 +6,24 @@ import { Color, Label } from 'ng2-charts';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  segmentChanged(ev: any) {
-    console.log('Segment changed', ev);
-  }
+  @Input() island_name1: any;
+  @Input() island_name2: any;
+  @Input() island_name3: any;
+  @Input() island_name4: any;
+  @Input() island_name5: any;
+  @Input() visit_count1: any;
+  @Input() visit_count2: any;
+  @Input() visit_count3: any;
+  @Input() visit_count4: any;
+  @Input() visit_count5: any;
+  @Input() isGraph: any;
+
+  apiBaseUrl: string = "http://localhost/";
+  apiUrl: string = this.apiBaseUrl+"/top5island_api/api/getTop5ByDate.php?d=";
 
   public barChartOptions:any = {
     scaleShowVerticalLines: false,
@@ -34,111 +45,103 @@ export class HomePage {
   public barChartLegend:boolean = false;
   
   public barChartData:any[] = [
-    {data: [65, 59, 80, 81, 56], label: 'Series A'},
+    {data: [0, 0, 0, 0, 0], label: 'visits'},
   ];
+
+  constructor(private http: HttpClient) {
+    // Update result for first time
+    this.updateResult('2017-03-01');
+  }
+
+  ngOnInit() {
+  }
+
+  public updateResult(selectedDate: string):void{
+
+    // Call API
+    this.http.get(this.apiUrl+selectedDate).subscribe(res => {
+    
+      // // Show API result
+      // const data = res;
+      // console.log(data);
+
+      // const history = [65, 22, 80, 81, 43];
+
+      // this.barChartData[0].data = [];
+
+      // for (let h of history) {
+      //   this.barChartData[0].data.push(h);
+      // }
+
+      // Show API result
+      const success = res['success'];
+      const message = res['message'];
+      const result = res['result'];
+      const islandNames = [];
+      const visitCount = [];
+ 
+      this.barChartLabels = [];
+      this.barChartData[0].data = [];
+ 
+      for (let r of result) {
+
+        // Update list
+        // this.island_name = r['island_name'];
+        // this.visit_count = r['visit_count'];
+        islandNames.push(r['island_name']);
+        visitCount.push(r['visit_count']);
+
+        // Update graph
+          this.barChartLabels.push(r['island_name']);
+          this.barChartData[0].data.push(r['visit_count']);
+      }
+
+      this.island_name1 = islandNames[0];
+      this.island_name2 = islandNames[1];
+      this.island_name3 = islandNames[2];
+      this.island_name4 = islandNames[3];
+      this.island_name5 = islandNames[4];
+      this.visit_count1 = visitCount[0] + " visits";
+      this.visit_count2 = visitCount[1] + " visits";
+      this.visit_count3 = visitCount[2] + " visits";
+      this.visit_count4 = visitCount[3] + " visits";
+      this.visit_count5 = visitCount[4] + " visits";
+
+    });
+  }
+
+  dateChanged(ev: any) {
+    
+    // Show date changed data
+    // console.log('Date changed', ev);
+
+    // Remove the timestamp
+    var selectedDate = ev.detail.value.split('T')[0]; 
+    // console.log(selectedDate);
+
+    // Update result each time date changed
+    this.updateResult(selectedDate);    
+  }
+
+  segmentChanged(ev: any) {
+    console.log('Segment changed', ev);
+    if(ev.detail.value=="graph")
+    {
+      this.isGraph = true; // Show graph 
+    }
+    else{
+      this.isGraph = false; // Show list
+
+    }
+  }
   
   // events
   public chartClicked(e:any):void {
-    console.log(e);
+    // console.log(e);
   }
   
   public chartHovered(e:any):void {
-    console.log(e);
+    // console.log(e);
   }
-  
-  public randomize():void {
-    // // Only Change 3 values
-    // let data = [
-    //   Math.round(Math.random() * 100),
-    //   59,
-    //   80,
-    //   (Math.random() * 100),
-    //   56,
-    //   (Math.random() * 100),
-    //   40];
-    // let clone = JSON.parse(JSON.stringify(this.barChartData));
-    // clone[0].data = data;
-    // this.barChartData = clone;
-    /**
-     * (My guess), for Angular to recognize the change in the dataset
-     * it has to change the dataset variable directly,
-     * so one way around it, is to clone the data, change it and then
-     * assign it;
-     */
-     
-    this.http.get('https://randomuser.me/api/').subscribe(res => {
-    
-      // Show API result
-      const data = res;
-      console.log(data);
-
-      const history = [65, 22, 80, 81, 43];
-
-      this.barChartData[0].data = [];
-
-      for (let h of history) {
-        this.barChartData[0].data.push(h);
-      }
-
-    });
-    
-  }
-
-  constructor(private http: HttpClient) {
-  }
-
-  // // Data
-  // chartData: ChartDataSets[] = [{ data: [], label: 'Stock price' }];
-  // chartLabels: Label[];
- 
-  // // Options
-  // chartOptions = {
-  //   responsive: true,
-  //   title: {
-  //     display: true,
-  //     text: 'Historic Stock pricee'
-  //   },
-  //   pan: {
-  //     enabled: true,
-  //     mode: 'xy'
-  //   },
-  //   zoom: {
-  //     enabled: true,
-  //     mode: 'xy'
-  //   }
-  // };
-  // chartColors: Color[] = [
-  //   {
-  //     borderColor: '#000000',
-  //     backgroundColor: '#ff00ff'
-  //   }
-  // ];
-  // chartType = 'line';
-  // showLegend = false;
- 
-  // // For search
-  // stock = '';
- 
-  // constructor(private http: HttpClient) {
-  // }
- 
-  // getData() {
-  //     this.http.get(`https://financialmodelingprep.com/api/v3/historical-price-full/${this.stock}?from=2018-03-12&to=2019-03-12`).subscribe(res => {
-  //     const history = res['historical'];
- 
-  //     this.chartLabels = [];
-  //     this.chartData[0].data = [];
- 
-  //     for (let entry of history) {
-  //       this.chartLabels.push(entry.date);
-  //       this.chartData[0].data.push(entry['close']);
-  //     }
-  //   });
-  // }
- 
-  // typeChanged(e) {
-  //   const on = e.detail.checked;
-  //   this.chartType = on ? 'line' : 'bar';
-  // }
 
 }
